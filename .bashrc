e8a57e0b34c722e238c8353c1947c16786916ca2
Siddharth Suresh condor <sidd.suresh97@gmail.com>
2022-11-14 23:18:38 -0600

    changes from condor

---
+++ .bashrc
@@ -0,0 +1,3 @@
+export HOME=/var/lib/condor/execute/slot1/dir_3003000
+export PATH
+export PATH=/var/lib/condor/execute/slot1/dir_3003000/miniconda3/bin:/var/lib/condor/execute/slot1/dir_3003000/miniconda3/bin:/usr/local/bin:/usr/bin
--- scripts/condor-job.sh
+++ scripts/condor-job.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/bash
 
-tar -xvf ecoset_leuven.tar.gz
 export HOME=$PWD
 export PATH
 sh vision_robustness_using_semantic_norms/Miniconda3-latest-Linux-x86_64.sh -b -p $PWD/miniconda3
@@ -11,9 +10,14 @@ export HOME=$PWD
 export PATH
 export PATH=$PWD/miniconda3/bin:$PATH
 EOF
-
+conda init bash
 conda create -n alexnet python=3.7
 conda activate alexnet
-pip install torch==1.10.1+cu111 -f https://download.pytorch.org/whl/torch_stable.html
-#pip install deepspeed
+pip install torch==1.10.1+cu111 torchvision==0.11.2+cu111 -f https://download.pytorch.org/whl/torch_stable.html
+# pip install deepspeed
 #pip install transformers
+
+
+
+tar -xvf ecoset_leuven.tar.gz
+sleep 7d
