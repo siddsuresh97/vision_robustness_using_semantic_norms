8cbe89d3240ef143bd9fcabf1455b0bb84a24d11
siddsuresh97 <sidd.suresh97@gmail.com>
2022-11-30 02:45:18 -0600

    fixed logging metrics on wandb

--- src/utils/metrics.py
+++ src/utils/metrics.py
@@ -1,18 +1,18 @@
 import numpy as np
 from sklearn.metrics import multilabel_confusion_matrix, classification_report
 from sklearn.metrics import precision_score, recall_score, f1_score
-def calculate_metrics(target, pred, threshold=0.5):
+def calculate_metrics(target, pred, dataset_type, threshold=0.5):
     pred = np.array(pred > threshold, dtype=float)
-    return {'micro/precision': precision_score(y_true=target, y_pred=pred, average='micro'),
-            'micro/recall': recall_score(y_true=target, y_pred=pred, average='micro'),
-            'micro/f1': f1_score(y_true=target, y_pred=pred, average='micro'),
-            'macro/precision': precision_score(y_true=target, y_pred=pred, average='macro'),
-            'macro/recall': recall_score(y_true=target, y_pred=pred, average='macro'),
-            'macro/f1': f1_score(y_true=target, y_pred=pred, average='macro'),
-            'samples/precision': precision_score(y_true=target, y_pred=pred, average='samples'),
-            'samples/recall': recall_score(y_true=target, y_pred=pred, average='samples'),
-            'samples/f1': f1_score(y_true=target, y_pred=pred, average='samples'),
-            'weighted/precision': precision_score(y_true=target, y_pred=pred, average='weighted'),
-            'weighted/recall': recall_score(y_true=target, y_pred=pred, average='weighted'),
-            'weighted/f1': f1_score(y_true=target, y_pred=pred, average='weighted')
+    return {'micro/{}_precision'.format(dataset_type): precision_score(y_true=target, y_pred=pred, average='micro'),
+            'micro/{}_recall'.format(dataset_type): recall_score(y_true=target, y_pred=pred, average='micro'),
+            'micro/{}_f1'.format(dataset_type): f1_score(y_true=target, y_pred=pred, average='micro'),
+            'macro/{}_precision'.format(dataset_type): precision_score(y_true=target, y_pred=pred, average='macro'),
+            'macro/{}_recall'.format(dataset_type): recall_score(y_true=target, y_pred=pred, average='macro'),
+            'macro/{}_f1'.format(dataset_type): f1_score(y_true=target, y_pred=pred, average='macro'),
+            'samples/{}_precision'.format(dataset_type): precision_score(y_true=target, y_pred=pred, average='samples'),
+            'samples/{}_recall'.format(dataset_type): recall_score(y_true=target, y_pred=pred, average='samples'),
+            'samples/{}_f1'.format(dataset_type): f1_score(y_true=target, y_pred=pred, average='samples'),
+            'weighted/{}_precision'.format(dataset_type): precision_score(y_true=target, y_pred=pred, average='weighted'),
+            'weighted/{}_recall'.format(dataset_type): recall_score(y_true=target, y_pred=pred, average='weighted'),
+            'weighted/{}_f1'.format(dataset_type): f1_score(y_true=target, y_pred=pred, average='weighted')
             }
